0f8e1d553d2263d9151e1d04c39c31e599259c2e
bricks <bricks@unomaha.edu>
2024-12-03 15:16:11 -0600

    Setup for class

--- p3.py
+++ p3.py
@@ -1,14 +1,11 @@
 from PIL import Image
 from io import StringIO
 
-image = Image.open("fish.jpg")
+image = Image.open("Christmas.jpg")
 data = image.load()
 
 ppm_file = StringIO()
-ppm_file.write("P3\n")
-ppm_file.write(str(image.width) + " ")
-ppm_file.write(str(image.height) + "\n")
-ppm_file.write("255\n")
+ppm_file.write(f"P3\n{image.width} {image.height}\n255\n")
 
 for y in range(image.height):
   for x in range(image.width):
@@ -16,9 +13,7 @@ for y in range(image.height):
     ppm_file.write(f"{pixel[0]} {pixel[1]} {pixel[2]} ")
   ppm_file.write("\n")
 
-
-
-with open("out.ppm", "w") as text_file:
+with open("P3.ppm", "w") as text_file:
   text_file.write(ppm_file.getvalue())
 
 ppm_values = ppm_file.getvalue().split()
@@ -44,16 +39,11 @@ data_out = image_out.load()
 
 for y in range(height):
   for x in range(width):
-    if x == 370 and y == 200:
-      pass
     r = int(ppm_values[index])
-    index += 1
-    g = int(ppm_values[index])
-    index += 1
-    b = int(ppm_values[index])
-    index += 1
+    g = int(ppm_values[index+1])
+    b = int(ppm_values[index+2])
+    index += 3
+    
+    data_out[x,y] = (r, g, b)
 
-    pixel = (r, g, b)
-    data_out[x,y] = pixel
-
-image_out.save("fish.png")
+image_out.save("P3.png")
