f7f5c2b070c07445591e3cd5c77a0beb9403009d
Ricks <bricks@unomaha.edu>
2024-12-03 04:36:39 -0600

    Now read P6

--- p6.py
+++ p6.py
@@ -1,65 +1,81 @@
 from PIL import Image
-from io import StringIO
 
-image = Image.open("debug.png")
+image = Image.open("cookies.jpg")
 data = image.load()
 
 ppm_file = bytearray()
-ppm_file += bytearray("P6\n".encode("utf-8"))
-ppm_file += bytearray((str(image.width) + " ").encode("utf-8"))
-ppm_file += bytearray((str(image.height) + " ").encode("utf-8"))
-ppm_file+= bytearray(("255\n").encode("utf-8"))
+ppm_file += bytearray(b"P6\n")
+ppm_file += bytearray(str(image.width).encode())
+ppm_file += b" "
+ppm_file += bytearray(str(image.height).encode())
+ppm_file += b" "
+ppm_file+= bytearray(b"255\n")
 
 for y in range(image.height):
   for x in range(image.width):
     pixel = data[x,y]
-    r = chr(pixel[0])
-    g = chr(pixel[0])
-    b = chr(pixel[0])
     ppm_file.append(pixel[0])
     ppm_file.append(pixel[1])
     ppm_file.append(pixel[2])
 
-
-
 with open("out.ppm", "wb") as text_file:
   text_file.write(ppm_file)
 
-quit()
-
-ppm_values = ppm_file.getvalue().split()
 
 index = 0
-
-front = ppm_values[index]
+assert chr(ppm_file[index]) == "P"
 index += 1
-assert front == "P6"
-
-width = int(ppm_values[index])
-index += 1
-height = int(ppm_values[index])
-index += 1
-_ = ppm_values[index]
+assert chr(ppm_file[index]) == "6"
+index +=1 
+assert chr(ppm_file[index]).isspace()
 index += 1
 
+#Get width
+buffer = []
+while  chr(ppm_file[index]).isspace() is False:
+  buffer += chr(ppm_file[index])
+  index += 1
+
+width = int("".join(buffer))
 print(width)
+
+# Move forward one since we found whitespace
+index+=1
+
+#Get height
+buffer = []
+while chr(ppm_file[index]).isspace() is False:
+  buffer += chr(ppm_file[index])
+  index += 1
+height = int("".join(buffer))
 print(height)
 
+# Move forward one since we found whitespace
+index+=1
+
+#Get size
+buffer = []
+while chr(ppm_file[index]).isspace() is False:
+  buffer += chr(ppm_file[index])
+  index += 1
+size = int("".join(buffer))
+print(size)
+
+# Move forward one since we found whitespace
+index+=1
+
+# Now read each byte
 image_out = Image.new("RGB", (width, height))
 data_out = image_out.load()
 
+
 for y in range(height):
   for x in range(width):
-    if x == 370 and y == 200:
-      pass
-    r = int(ppm_values[index])
-    index += 1
-    g = int(ppm_values[index])
-    index += 1
-    b = int(ppm_values[index])
-    index += 1
+    r = int(ppm_file[index])
+    g = int(ppm_file[index+1])
+    b = int(ppm_file[index+2])
+    index += 3
+    data_out[x,y] = (r,g,b)
 
-    pixel = (r, g, b)
-    data_out[x,y] = pixel
+image_out.save("done.png")
 
-image_out.save("fish.png")
