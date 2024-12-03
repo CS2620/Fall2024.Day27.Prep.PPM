776c0e8f91907cf17c6b1963c0348e2379b308a3
Ricks <bricks@unomaha.edu>
2024-12-03 04:48:48 -0600

    Improve readability

--- p6.py
+++ p6.py
@@ -3,13 +3,10 @@ from PIL import Image
 image = Image.open("cookies.jpg")
 data = image.load()
 
-ppm_file = bytearray()
-ppm_file += bytearray(b"P6\n")
-ppm_file += bytearray(str(image.width).encode())
-ppm_file += b" "
-ppm_file += bytearray(str(image.height).encode())
-ppm_file += b" "
-ppm_file+= bytearray(b"255\n")
+
+header = f"P6\n{image.width}\n{image.height}\n255\n"
+
+ppm_file = bytearray(header.encode())
 
 for y in range(image.height):
   for x in range(image.width):
@@ -21,44 +18,44 @@ for y in range(image.height):
 with open("out.ppm", "wb") as text_file:
   text_file.write(ppm_file)
 
+#Decode our P6 file
 
-index = 0
-assert chr(ppm_file[index]) == "P"
-index += 1
-assert chr(ppm_file[index]) == "6"
-index +=1 
-assert chr(ppm_file[index]).isspace()
-index += 1
+assert chr(ppm_file[0]) == "P"; 
+assert chr(ppm_file[1]) == "6"; 
+assert chr(ppm_file[2]).isspace(); 
 
+index = 3
 #Get width
-buffer = []
-while  chr(ppm_file[index]).isspace() is False:
-  buffer += chr(ppm_file[index])
+width_string = ""
+while  chr(ppm_file[index]).isdigit():
+  width_string += chr(ppm_file[index])
   index += 1
 
-width = int("".join(buffer))
+width = int(width_string)
 print(width)
 
 # Move forward one since we found whitespace
 index+=1
 
 #Get height
-buffer = []
-while chr(ppm_file[index]).isspace() is False:
-  buffer += chr(ppm_file[index])
+height_string = ""
+while chr(ppm_file[index]).isdigit():
+  height_string += chr(ppm_file[index])
   index += 1
-height = int("".join(buffer))
+  
+height = int(height_string)
 print(height)
 
 # Move forward one since we found whitespace
 index+=1
 
 #Get size
-buffer = []
-while chr(ppm_file[index]).isspace() is False:
-  buffer += chr(ppm_file[index])
+size_string = ""
+while chr(ppm_file[index]).isdigit():
+  size_string += chr(ppm_file[index])
   index += 1
-size = int("".join(buffer))
+
+size = int(size_string)
 print(size)
 
 # Move forward one since we found whitespace
@@ -68,14 +65,12 @@ index+=1
 image_out = Image.new("RGB", (width, height))
 data_out = image_out.load()
 
-
 for y in range(height):
   for x in range(width):
-    r = int(ppm_file[index])
-    g = int(ppm_file[index+1])
-    b = int(ppm_file[index+2])
+    r = ppm_file[index]
+    g = ppm_file[index+1]
+    b = ppm_file[index+2]
     index += 3
     data_out[x,y] = (r,g,b)
 
 image_out.save("done.png")
-
